9fb3b6f2ce322ef55c7322636202e9ab451f40dd
Deepak Kumar Morya <morya3283@gmail.com>
2026-07-25 04:12:49 +0530

    Change birthday to September 3, 2025
    Change birthday date

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   title: 'A Special Surprise',
-  birthday: '2026-09-03T00:00:00+05:30',
+  birthday: '2025-09-03T00:00:00+05:30',
   recipientName: 'Someone Special',
 } as const;
